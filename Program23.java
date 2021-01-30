00be918398b88e606709c074c91773a4a487a1ef
Nitinpal269 <76909634+Nitinpal269@users.noreply.github.com>
2021-01-30 23:31:09 +0530

    Star Pattern

---
+++ Program23.java
@@ -0,0 +1,24 @@
+class Program23
+{
+	public static void main(String[] args)
+	{
+		for(int i=1; i<=6; i++)
+		{
+				int k=1;
+			for(int j=1; j<=6; j++)
+				if(j<=i)
+				{
+					System.out.print(k);
+					k=1-k;
+				
+				}
+		
+				else
+					System.out.print(" ");
+		
+		System.out.println();
+		}
+	}
+}
+
+		
